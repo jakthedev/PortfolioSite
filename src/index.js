97a1e3c59b91aa44c072de15a772d82508d2fe71
JD Alexander Kemp <jdakemp@gmail.com>
2020-10-09 15:50:27 -0400

    Web App

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
+//registerServiceWorker();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
